f71e00d82627cf932fcc1e78270aa98cd133a9b3
vinay <sterlingvinay01@gmail.com>
2024-01-16 22:37:01 -0500

    new-config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ca-central-1_zzpB11i2N', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '18d7ce8nifki2j62anu28kdeh8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ca-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
